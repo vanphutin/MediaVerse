4a9580f417fdb0e77a4bf78ad6f5a4e626c0f75b
vanphutin2004 <vanphutin.contact@gmail.com>
2025-11-25 10:50:08 +0700

    feat: add validation & exception handling for backend

--- backend-service/src/main/java/com/vanphutin/controller/request/UserCreationRequest.java
+++ backend-service/src/main/java/com/vanphutin/controller/request/UserCreationRequest.java
@@ -1,5 +1,7 @@
 package com.vanphutin.controller.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.io.Serializable;
@@ -9,7 +11,10 @@ import java.util.List;
 @Data
 public class UserCreationRequest implements Serializable {
 
+    @NotBlank(message = "username must be not blank")
     private String username;
+
+    @Email(message = "Email invalid")
     private String email;
     private String bio;
     private String location;
--- backend-service/src/main/java/com/vanphutin/controller/request/UserPasswordRequest.java
+++ backend-service/src/main/java/com/vanphutin/controller/request/UserPasswordRequest.java
@@ -1,12 +1,22 @@
 package com.vanphutin.controller.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
 public class UserPasswordRequest implements Serializable {
+
+    @NotNull(message = "id must be not blank")
+    @Min(value = 1, message = "userId must be equal or greater than 1")
     private Long id;
+
+    @NotBlank(message = "password must be not blank")
     private String password;
+
+    @NotBlank(message = "confirmPassword must be not blank")
     private String confirmPassword;
 }
